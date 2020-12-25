50aa23d7e933c526d2a10d9ee3bb442cc6d2422c
KaluW_ <jrice7105@gmail.com>
2020-12-25 15:21:48 -0700

    fixed annoying indentation in data.c?

--- src/data.c
+++ src/data.c
@@ -13,25 +13,25 @@
 block_t blocks[MAX_BLOCK_TILE_COUNT] = {
 	
 //	 (Single) Yield		Required Tool			Best Tool		Hardness		onClick						onUpdate			
-	{ NO_YIELD, 		NO_TOOL, 				None, 			NO_HARDNESS, 	NULL, 						NULL					}, // AIR
-    { COBBLESTONE, 		WOODEN_PICKAXE, 		Pickaxe, 		10, 			NULL, 						NULL					}, // STONE
-    { DIRT, 			NO_TOOL, 				Shovel, 		3, 				NULL, 						NULL					}, // GRASS
-    { DIRT, 			NO_TOOL, 				Shovel, 		2, 				NULL, 						NULL					}, // DIRT
-	{ COBBLESTONE,		WOODEN_PICKAXE,			Pickaxe,		10,				NULL, 						NULL					}, // COBBLESTONE
-	{ COAL_ORE,			WOODEN_PICKAXE,			Pickaxe,		15,				NULL, 						NULL					}, // COAL_ORE
-	{ DIAMOND_ORE,		IRON_PICKAXE,			Pickaxe,		15,				NULL, 						NULL					}, // DIAMOND_ORE
-	{ GOLD_ORE,			IRON_PICKAXE,			Pickaxe,		15,				NULL, 						NULL					}, // GOLD_ORE
-	{ IRON_ORE,			STONE_PICKAXE,			Pickaxe,		15,				NULL, 						NULL					}, // IRON_ORE
-	{ NO_YIELD, 		NO_TOOL, 				None, 			NO_HARDNESS, 	NULL, 						NULL					}, // BEDROCK
-	{ OAK_SAPLING,		NO_TOOL,				None,			NO_HARDNESS,	NULL, 						NULL					}, // OAK_SAPLING
-	{ OAK_LOG,			NO_TOOL,				Axe,			10,				NULL, 						NULL					}, // OAK_LOG
-	{ OAK_PLANK,		NO_TOOL,				Axe,			10,				NULL, 						NULL					}, // OAK_PLANK
-	{ LEAVES,			SHEARS,					Shears,			1,				NULL, 						NULL					}, // LEAVES
-	{ SAND,				NO_TOOL,				None,			2,				NULL,						gravityBlock_onUpdate 	}, // SAND
-	{ SANDSTONE,		WOODEN_PICKAXE,			Pickaxe,		4,				NULL, 						NULL					}, // SANDSTONE
-	{ CACTUS,			NO_TOOL,				None,			3,				equalEfficiency_onClick, 	NULL					}, // CACTUS
+	{ NO_YIELD,			NO_TOOL,				None,			NO_HARDNESS,	NULL,						NULL					}, // AIR
+	{ COBBLESTONE,		WOODEN_PICKAXE,			Pickaxe,		10,				NULL,						NULL					}, // STONE
+	{ DIRT,				NO_TOOL,				Shovel,			3,				NULL,						NULL					}, // GRASS
+	{ DIRT,				NO_TOOL,				Shovel,			2,				NULL,						NULL					}, // DIRT
+	{ COBBLESTONE,		WOODEN_PICKAXE,			Pickaxe,		10,				NULL,						NULL					}, // COBBLESTONE
+	{ COAL_ORE,			WOODEN_PICKAXE,			Pickaxe,		15,				NULL,						NULL					}, // COAL_ORE
+	{ DIAMOND_ORE,		IRON_PICKAXE,			Pickaxe,		15,				NULL,						NULL					}, // DIAMOND_ORE
+	{ GOLD_ORE,			IRON_PICKAXE,			Pickaxe,		15,				NULL,						NULL					}, // GOLD_ORE
+	{ IRON_ORE,			STONE_PICKAXE,			Pickaxe,		15,				NULL,						NULL					}, // IRON_ORE
+	{ NO_YIELD,			NO_TOOL,				None,			NO_HARDNESS,	NULL,						NULL					}, // BEDROCK
+	{ OAK_SAPLING,		NO_TOOL,				None,			NO_HARDNESS,	NULL,						NULL					}, // OAK_SAPLING
+	{ OAK_LOG,			NO_TOOL,				Axe,			10,				NULL,						NULL					}, // OAK_LOG
+	{ OAK_PLANK,		NO_TOOL,				Axe,			10,				NULL,						NULL					}, // OAK_PLANK
+	{ LEAVES,			SHEARS,					Shears,			1,				NULL,						NULL					}, // LEAVES
+	{ SAND,				NO_TOOL,				None,			2,				NULL,						gravityBlock_onUpdate	}, // SAND
+	{ SANDSTONE,		WOODEN_PICKAXE,			Pickaxe,		4,				NULL,						NULL					}, // SANDSTONE
+	{ CACTUS,			NO_TOOL,				None,			3,				equalEfficiency_onClick,	NULL					}, // CACTUS
 	{ CRAFTING_TABLE,	NO_TOOL,				Axe,			12,				craftingTable_onClick,		NULL					}, // CRAFTING_TABLE
-	{ CHEST, 			NO_TOOL,				Axe,			21,				chest_onClick,				NULL					}, // CHEST
+	{ CHEST,			NO_TOOL,				Axe,			21,				chest_onClick,				NULL					}, // CHEST
 	{ FURNACE,			WOODEN_PICKAXE,			Pickaxe,		17,				furnace_onClick,			NULL					}, // FURNACE_OFF
 	{ FURNACE,			WOODEN_PICKAXE,			Pickaxe,		17,				furnace_onClick,			NULL					}, // FURNACE_ON
 	{ NO_YIELD,			NO_TOOL,				None,			NO_HARDNESS,	deadBush_onClick,			NULL					}, // DEAD_BUSH
@@ -43,8 +43,8 @@ block_t blocks[MAX_BLOCK_TILE_COUNT] = {
 	{ OAK_DOOR,			NO_TOOL,				Axe,			15,				door_onClick,				NULL					}, // OAK_DOOR_BOTTOM
 	{ OAK_DOOR,			NO_TOOL,				Axe,			15,				door_onClick,				NULL					}, // OAK_DOOR_TOP
 	{ LADDER,			NO_TOOL,				Axe,			2,				NULL,						NULL					}, // LADDER
-	{ NO_YIELD, 		NO_TOOL, 				None, 			NO_HARDNESS, 	water_onClick,				water_onUpdate 			}, // WATER
-	{ NO_YIELD, 		NO_TOOL, 				None, 			NO_HARDNESS, 	lava_onClick,				lava_onUpdate 			} // LAVA
+	{ NO_YIELD,			NO_TOOL,				None,			NO_HARDNESS,	water_onClick,				water_onUpdate			}, // WATER
+	{ NO_YIELD,			NO_TOOL,				None,			NO_HARDNESS,	lava_onClick,				lava_onUpdate			} // LAVA
 	
 };
 
